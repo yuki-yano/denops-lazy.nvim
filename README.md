52b8db6efa6f70fe54307c3ddae18aace5c284be
yuki_yano <yuki.ycino@gmail.com>
2023-01-17 22:12:51 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# denops-lazy.nvim
+
+Plugin for lazy load of [denops.vim](https://github.com/vim-denops/denops.vim) plugin with [lazy.nvim](https://github.com/folke/lazy.nvim).
+
+## Usage
+
+Execute the `load` function of denops-lazy.nvim in the `config` property of lazy.nvim.
+
+```lua
+return {
+  {
+    'yuki-yano/fuzzy-motion.vim',
+    dependencies = {
+      { 'vim-denops/denops.vim' },
+    },
+    cmd = { 'FuzzyMotion' },
+    config = function()
+      require('denops-lazy').load('fuzzy-motion.vim')
+    end,
+  },
+}
+```
+
+## Option
+
+The second argument of load specifies options for loading.
+
+### `wait_load`
+
+Run `denops#plugin#wait` after loading a plugin and wait for the plugin to complete loading.
+
+default: `true`
+
+### `wait_server`
+
+Wait for startup if the denops server has not completed startup.
+
+default: `true`
+
+### `wait_server_delay`
+
+Time to wait for the denops to start (milliseconds)
+
+default: `10`
+
+### `wait_server_retry_limit`
+
+Number of times to wait for denops to start
+
+default: `100`
---
+++ lua/denops-lazy/init.lua
@@ -0,0 +1,78 @@
+local M = {}
+
+---@class denops-lazy.Opts
+---@field public wait_load? boolean # default: true
+---@field public wait_server? boolean # default: true
+---@field public wait_server_delay? number # default: 10
+---@field public wait_server_retry_limit? number # default:100
+
+---@type denops-lazy.Opts
+local default_opts = {
+  wait_load = true,
+  wait_server = true,
+  wait_server_delay = 10,
+  wait_server_retry_limit = 100,
+}
+
+---@param opts denops-lazy.Opts
+local function wait_server(opts)
+  local once = false
+  for _ = 1, opts.wait_server_retry_limit do
+    if vim.fn['denops#server#status']() == 'running' then
+      return
+    end
+    if not once then
+      require('lazy').load({ plugins = 'denops.vim' })
+      vim.fn['denops#server#start']()
+      once = true
+    end
+    vim.wait(opts.wait_server_delay)
+  end
+end
+
+---@param plugin_name string
+---@param opts denops-lazy.Opts
+M.load = function(plugin_name, opts)
+  require('lazy').load({ plugins = plugin_name })
+
+  ---@type denops-lazy.Opts
+  opts = vim.tbl_extend('force', default_opts, opts or {})
+
+  local plugins = require('lazy').plugins()
+  local plugin = nil
+  for _, p in ipairs(plugins) do
+    if p.name == plugin_name then
+      plugin = p
+      break
+    end
+  end
+
+  if not plugin or vim.fn.isdirectory(plugin.dir .. '/' .. 'denops') == 0 then
+    return
+  end
+
+  local denops_plugins = vim.fn.globpath(plugin.dir, 'denops/*/main.ts', true, true)
+
+  if not #denops_plugins == 0 then
+    if opts.wait_server then
+      wait_server(opts)
+    end
+  end
+
+  if not vim.fn['denops#server#status']() == 'running' then
+    return
+  end
+
+  for _, denops_plugin in ipairs(denops_plugins) do
+    denops_plugin = vim.fn.fnamemodify(denops_plugin, ':h:t')
+
+    if vim.fn['denops#plugin#is_loaded'](denops_plugin) == 0 then
+      pcall(vim.fn['denops#plugin#register'], denops_plugin, { mode = 'skip' })
+      if opts.wait_load then
+        vim.fn['denops#plugin#wait'](denops_plugin)
+      end
+    end
+  end
+end
+
+return M
